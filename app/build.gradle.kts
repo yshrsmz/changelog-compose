4609a7b0b071bd9fb7fb80a718a42504e275d63b
Yasuhiro Shimizu <the.phantom.bane+github@gmail.com>
2025-10-30 23:35:14 +0900

    feat: lower minSdk to 24 (#3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.codingfeline.changelog.sample"
-        minSdk = 29
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
